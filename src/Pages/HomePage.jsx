85e4d8886462fdee054b72ae01c13c55ada7029c
jeffersondeolveira <jeffersondeoliveira004@gmail.com>
2024-07-19 14:56:06 -0300

    Mudando o width do footer e Ofert Terminado

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
+import Oferta from '../Components/Oferta/Oferta'
 import Footer from '../Components/Footer/Footer'
 function HomePage() {
 
   return (
     <>
-
+      <Oferta></Oferta>
       <Footer></Footer>
 
     </>
