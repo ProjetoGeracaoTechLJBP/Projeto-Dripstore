fce67920106c0148c578b13315ea0deb7cf34a70
jeffersondeoliveira <jeffersondeoliveira004@gmail.com>
2024-07-21 14:20:17 -0300

    Adicionado Bootstrap e Modificando o componente.

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import Footer from '../Components/Footer/Footer'
+import Produtos from '../Components/Produtos/Produtos'
 function HomePage() {
 
   return (
     <>
-
-      <Footer></Footer>
-
+      <Produtos></Produtos>
+{/*       <Footer></Footer>
+ */}
     </>
   )
 }
