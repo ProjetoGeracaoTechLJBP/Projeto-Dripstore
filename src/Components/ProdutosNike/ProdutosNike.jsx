f7ce8e33bc3de6a5fdffd3951aee6d466c8a0ff4
jeffersondeoliveira <jeffersondeoliveira004@gmail.com>
2024-07-23 00:17:02 -0300

    Sprint 3 em produção, botões atualizados

--- src/Components/ProdutosNike/ProdutosNike.jsx
+++ src/Components/ProdutosNike/ProdutosNike.jsx
@@ -1,12 +1,13 @@
 import TenisNike from '../../assets/tenisNike.png'
+import PrecoValue from '../../assets/PrecoValue.png'
 import './ProdutosNike.css'
-import "bootstrap/dist/css/bootstrap.min.css";
+/* import "bootstrap/dist/css/bootstrap.min.css"; */
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 function DarkVariantExample() {
   return (
     <>
-    <div id='Sect-Nav'>
+      <div id='Sect-Nav'>
         <p>Home /</p>
         <p>Produto /</p>
         <p>Tênis /</p>
@@ -14,83 +15,99 @@ function DarkVariantExample() {
       </div>
 
 
-    <section id='Sect-main'>
-      <section id='Sect-TenisAndInformation'>
+      <section id='Sect-main'>
+        <section id='Sect-TenisAndInformation'>
 
-      <div id="carouselExampleDark" className="carousel carousel-dark slide">
-        <div className="carousel-indicators">
-          <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" className="active"
-            aria-current="true" aria-label="Slide 1"></button>
-          <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1"
-            aria-label="Slide 2"></button>
-          <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2"
-            aria-label="Slide 3"></button>
-        </div>
-        <div className="carousel-inner">
-          <div className="carousel-item active" data-bs-interval="10000">
-            <div className="carousel-item-row">
-              <div className="imagem-ilustrativa">
-                <img src={TenisNike} className="d-block sapato" alt="sapato" />
+          <div id="carouselExampleDark" className="carousel carousel-dark slide">
+            <div className="carousel-indicators">
+              <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" className="active"
+                aria-current="true" aria-label="Slide 1"></button>
+              <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1"
+                aria-label="Slide 2"></button>
+              <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2"
+                aria-label="Slide 3"></button>
+            </div>
+            <div className="carousel-inner">
+              <div className="carousel-item active" data-bs-interval="10000">
+                <div className="carousel-item-row">
+                  <div className="imagem-ilustrativa">
+                    <img src={TenisNike} className="d-block sapato" alt="sapato" />
+                  </div>
+                </div>
+              </div>
+              <div className="carousel-item" data-bs-interval="2000">
+                <div className="carousel-item-row">
+                  <div className="imagem-ilustrativa">
+                    {<img src={TenisNike} className="d-block sapato" alt="sapato" />}
+                  </div>
+                </div>
+              </div>
+              <div className="carousel-item" data-bs-interval="2000">
+                <div className="carousel-item-row">
+                  <div className="imagem-ilustrativa">
+                    {<img src={TenisNike} className="d-block sapato" alt="sapato" />}
+                  </div>
+                </div>
               </div>
             </div>
+            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark"
+              data-bs-slide="prev">
+              <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span className="visually-hidden">Previous</span>
+            </button>
+            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleDark"
+              data-bs-slide="next">
+              <span className="carousel-control-next-icon" aria-hidden="true"></span>
+              <span className="visually-hidden">Next</span>
+            </button>
           </div>
-          <div className="carousel-item" data-bs-interval="2000">
-            <div className="carousel-item-row">
-              <div className="imagem-ilustrativa">
-                <img src={TenisNike} className="d-block sapato" alt="sapato" />
-              </div>
-            </div>
-          </div>
-          <div className="carousel-item" data-bs-interval="2000">
-            <div className="carousel-item-row">
-              <div className="imagem-ilustrativa">
-                <img src={TenisNike} className="d-block sapato" alt="sapato" />
-              </div>
-            </div>
-          </div>
-        </div>
-        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark"
-          data-bs-slide="prev">
-          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleDark"
-          data-bs-slide="next">
-          <span className="carousel-control-next-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Next</span>
-        </button>
-      </div>
 
 
-        <div id='Information-Product'>
+          <div id='Information-Product'>
             <div id='Part-Text'>
-              <h1>Tênis Nike Revolution 6 Next Nature Masculine</h1>
+              <h1>Tênis Nike Revolution<br /> 6  Next Nature Masculine</h1>
+              <p>Casual | Nike | REF:38416711</p>
             </div>
 
-          {/* Estrelas funcionais */}
-          <div>
 
-          </div>
+
+
+            {/* Estrelas funcionais */}
+            <div>
+
+            </div>
 
             <div id='Part-Price'>
-              <p>R$</p>
-              <p>219,</p>
-              <p>00</p>
-              <p>219,00</p>
+              <div id='Cifrao'>
+                <img src={PrecoValue} alt="" /> 
+                <div id='Mine-Value'>
+                  <p>219,00</p>  
+                </div>
+              </div>
+
             </div>
             <div id='Description'>
-              <p>Descrição</p>
-              <p>Apresentamos o Nike , o parceiro perfeito para suas corridas diárias. Com tecnologia de amortecimento responsivo e um design respirável, este tênis proporciona conforto e suporte excepcionais a cada passo. Prepare-se para voar nas pistas com estilo e desempenho incomparáveis.</p>
+              <p id='paragraph-Description'>Descrição do produto</p>
+              <p id='paragraph-DescriptionProduto'>
+              Os tênis da Nike combinam estilo e desempenho superior, proporcionando conforto e suporte incomparáveis para qualquer atividade física. Experimente o melhor em calçados esportivos com a Nike.</p>
             </div>
-            <p>Tamanho</p>
-            <div id='btn'>
+            <p id='Text-Tamanho'>Tamanho</p>
+            <div id='btn-Number'>
+              <button>39</button>
+              <button>40</button>
+              <button>41</button>
+              <button>42</button>
+              <button>43</button>
+            </div>
+            <p id='Text-Color'>Cores</p>
+            <div id='btn-Color'>
               <button>fafdfd</button>
-              <button>fafdfd</button>  
+              <button>fafdfd</button>
               <button>fafdfd</button>
               <button>fafdfd</button>
             </div>
-          <button>Comprar</button>
-        </div>
+            <button id='btn-Compra'>Comprar</button>
+          </div>
         </section>
       </section>
     </>
@@ -98,3 +115,10 @@ function DarkVariantExample() {
 }
 
 export default DarkVariantExample;
+
+
+{/* <p>R$</p>
+               </div>
+               <p id='Preco-Value'>219,</p>
+               <p id='Zero'>00</p>
+                */}
