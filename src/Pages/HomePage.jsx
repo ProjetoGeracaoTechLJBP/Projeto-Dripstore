c75f62d5b18dd6af64c9e0435afab46753a9bb0d
Paulohco47 <paulo.hco47@gmail.com>
2024-07-25 10:44:49 -0300

    carrosel completo

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
 import Footer from '../Components/Footer/Footer'
+import Carrossel from '../Components/Carrossel/Carrossel'
 function HomePage() {
 
   return (
     <>
-
+      <Carrossel></Carrossel>
       <Footer></Footer>
 
     </>
