e59b5e2c9bdcf844f0dd47a555f0c23bd8c459ea
beatrxzw <bealop53@gmail.com>
2024-07-25 12:13:52 -0300

    pre final carrinho

--- src/Components/Meu Carrinho/Contador.jsx
+++ src/Components/Meu Carrinho/Contador.jsx
@@ -1,4 +1,3 @@
-import Carrinho from "./Carrinho";
 import React, { useState, useEffect } from 'react';
 
 
@@ -6,21 +5,21 @@ export function Contador(initialValue = 1) {
     const [count, setCount] = useState(initialValue)
     const [multi, setMult] = useState(219)
 
-    function inc(){
+    function inc() {
         setCount(count + initialValue)
     }
 
-    function dec(){
+    function dec() {
         setCount(count - initialValue)
     }
 
     useEffect(() => {
-        setMult(() => 
-            count*219
+        setMult(() =>
+            count * 219
         )
     }, [count])
 
 
-    return[count, inc, dec, multi]
+    return [count, inc, dec, multi]
 }
 
--- src/Pages/CarrinhoPage.jsx
+++ src/Pages/CarrinhoPage.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import Footer from '../Components/Footer/Footer'
+import Header from '../Components/Header/Header'
+import Nav from '../Components/Nav/Nav'
 import Carrinho from "../Components/Meu Carrinho/Carrinho"
 function CarrinhoPage() {
 
   return (
     <>
-   <Carrinho></Carrinho>
-   <Footer></Footer>
+      <Header></Header>
+      <Nav></Nav>
+      <Carrinho></Carrinho>
+      <Footer></Footer>
 
     </>
   )
