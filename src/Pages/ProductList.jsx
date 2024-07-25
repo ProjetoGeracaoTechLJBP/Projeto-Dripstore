173c588b34baeb036496e47269736c73cadfbb57
Levy <85988919231levy@gmail.com>
2024-07-25 17:26:52 -0300

    Implementação da parte main da tela  productlist

---
+++ src/Pages/ProductList.jsx
@@ -0,0 +1,17 @@
+import Filtragem from '../Components/Filtragem/Filtragem'
+import ListProduct from '../Components/ListProduct/ListProduct'
+import Footer from '../Components/Footer/Footer'
+
+function ProductList() {
+    return(
+        <>
+            
+            
+            <Filtragem/>
+            <ListProduct/>
+            <Footer></Footer>
+            
+        </>
+    )
+}
+export default ProductList
