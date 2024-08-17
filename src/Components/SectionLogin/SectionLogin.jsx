16c9994311be9fe84c4541f8369919ec9d66a996
beatrixzw <bealop53@gmail.com>
2024-08-16 23:18:35 -0300

    :sparkles: feat: ação login finalizada

--- src/Components/SectionLogin/SectionLogin.jsx
+++ src/Components/SectionLogin/SectionLogin.jsx
@@ -1,14 +1,53 @@
 import gmail from "../../assets/gmail.png"
 import facebook from "../../assets/Original.png"
 import sapatos from "../../assets/sapatosCriarConta.png"
+import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom'
 import "./SectionLogin.css"
-function SectionLogin() {
-    return(
-        <div className="SectionLogin">
 
-            <div className="FormLogin"> 
-                <form >
+
+
+function SectionLogin() {
+    const navigate = useNavigate();
+
+    const handleLogin = async (event) => {
+        event.preventDefault();
+
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+
+        try {
+            const response = await fetch('http://localhost:3000/api/login', {
+                body: JSON.stringify({ email, password }),
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            });
+
+            // Vê se deu certo 
+            if (!response.ok) {
+                throw new Error(`Ocorreu erro na requisição: ${response.status}`);
+            }
+
+            // Converter a resposta para JSON
+            const data = await response.json();
+            console.log('API: ', data);
+
+            // Vai armazenar os dados
+            localStorage.setItem('user', JSON.stringify(data));
+
+            // Vai pra Home
+            navigate('/homeuser'); 
+
+        } catch (error) {
+            console.error('Erro na requisição:', error);
+        }
+    };
+    return (
+        <div className="SectionLogin">
+            <div className="FormLogin">
+                <form onSubmit={handleLogin} >
                     <div className="TitleFormLogin">
                         <h1>Acesse sua conta</h1>
                         {/* <p>Novo cliente? Então registre-se <a href="#">aqui</a></p> */}
@@ -17,12 +56,12 @@ function SectionLogin() {
 
                     <div className="InputLogin">
                         <label>Login *</label>
-                        <input type="email" placeholder="Insira seu login ou email"/>
+                        <input type="email" placeholder="Insira seu login ou email" name="email" />
                     </div>
 
                     <div className="InputSenhaLogin">
                         <label>Senha *</label>
-                        <input type="password"  placeholder="Insira sua senha"/>
+                        <input type="password" placeholder="Insira sua senha" name="password" />
                     </div>
 
                     <div className="EsqueciLogin">
@@ -41,11 +80,13 @@ function SectionLogin() {
 
                 </form>
             </div>
-            
+
             <div className="container_ImgSapatos">
                 <img className="ImgSapatos" src={sapatos} alt="" />
             </div>
         </div>
-    ) 
+    )
 }
+
 export default SectionLogin
+
